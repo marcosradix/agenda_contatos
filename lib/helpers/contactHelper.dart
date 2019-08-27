0c7405dc1b1148c6ec84ecba758454b5f60aa742
Marcos Ferreira <marcosferreira@MacBook-Pro-de-mac.local>
2019-08-27 19:27:37 -0300

    criada class de contato para entidade

--- lib/helpers/contactHelper.dart
+++ lib/helpers/contactHelper.dart
@@ -1,13 +1,43 @@
 import 'package:sqflite/sqflite.dart';
 
-class ConcatHelper {
+final String idColumn = "idColumn";
+final String nameColumn = "nameColumn";
+final String emailColumn = "emailColumn";
+final String phoneColumn = "phoneColumn";
+final String imgColumn = "imgColumn";
 
-}
+class ConcatHelper {}
 
 class Contact {
+  int id;
   String nome;
   String email;
   String phone;
   String img;
 
+  Contact.fromMap(Map map) {
+    id = map[idColumn];
+    nome = map[nameColumn];
+    email = map[emailColumn];
+    phone = map[phoneColumn];
+    img = map[imgColumn];
+  }
+
+  Map toMap() {
+    Map<String, dynamic> map = {
+      nameColumn : nome,
+      emailColumn : email,
+      phoneColumn : phone,
+      imgColumn : img
+    };
+    if(id != null){
+      map[idColumn] = id;
+    }
+    return map;
+  }
+
+  @override
+  String toString() {
+    return "Contato[id: $id, nome: $nome, email: $email, phone: $phone, img: $img]";
+  }
 }
